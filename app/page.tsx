f16fa2ff2f9f0157880a34d42353dc799163c34a
Gokul Prathin A <gokulprathin8@gmail.com>
2024-06-12 19:52:24 -0400

    changes to add formbricks

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,15 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import formbricks from "@formbricks/js/app";
+
+if (typeof window !== "undefined") {
+    formbricks.init({
+        environmentId: "",
+        apiHost: "",
+        userId: "",
+    });
+}
 
 export default function Home() {
   return (
