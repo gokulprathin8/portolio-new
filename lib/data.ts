0f9a86b0f6f97e27ee1cb0f7287320d6cd1aa4b3
Gokul Prathin <gokulprathin8@gmail.com>
2025-08-17 18:14:07 -0400

    added blog link to the header

--- lib/data.ts
+++ lib/data.ts
@@ -31,6 +31,10 @@ export const links = [
     name: "Contact",
     hash: "#contact",
   },
+  {
+    name: "Blog",
+    hash: "https://gokulprathin8.github.io/gokuls-blog/blog/",
+  }
 ] as const;
 
 export const experiencesData = [
